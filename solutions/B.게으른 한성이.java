e37e063285e61203b60143c1496a62fee5541ab5
junhaa <94986147+junhaa@users.noreply.github.com>
2023-11-08 05:05:20 +0900

    Update B.게으른 한성이.java

--- solutions/B.게으른 한성이.java
+++ solutions/B.게으른 한성이.java
@@ -9,12 +9,12 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int L = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        if(L - N < N){ // 엘리베이터를 이용한 후 계단을 내려가야 하는 거리 (L - N)이 1층부터 계단을 올라가야 하는 거리(N) 보다 작은 경우
+        if(L - N < N - 1){ // 엘리베이터를 이용한 후 계단을 내려가야 하는 거리 (L - N)이 1층부터 계단을 올라가야 하는 거리(N) 보다 작은 경우
             System.out.println(L - N);
         }
         else{ // 1층부터 계단을 올라가야 하는 층수가 더 적거나 같은 경우
-            System.out.println(N);
+            System.out.println(N - 1);
         }
-        //System.out.println(Math.min(L - N, N)); // Math.min을 이용하여 출력해도 됩니다.
+        //System.out.println(Math.min(L - N, N - 1)); // Math.min을 이용하여 출력해도 됩니다.
     }
 }
