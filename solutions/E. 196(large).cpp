0b5b7f0d9d67035c976bae8ccd61afe84e3ec268
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-11-09 03:10:40 +0000

    Update problem

--- solutions/E. 196(large).cpp
+++ solutions/E. 196(large).cpp
@@ -6,21 +6,26 @@
     pb : E. 196(large)
 */
 using namespace std;
-int main(){
+int main() {
     fast;
-    int T;cin >> T;
-    for(int Case=1;Case<=T;Case++){
-        int N;cin >> N;
-        unordered_map<long long, int>Numbers;
-        int cnt=0;
-        while(N--){//N
-            long long num;cin >> num;
+    int T;
+    cin >> T;
+    for (int Case = 1; Case <= T; Case++) {
+        int N;
+        cin >> N;
+        unordered_map<long long, int> Numbers;
+        int cnt = 0;
+        while (N--) {//N
+            long long num;
+            cin >> num;
+
+            if (Numbers.count(196 - num))
+                cnt += Numbers[196 - num];
+
             if (Numbers.count(num)) Numbers[num]++;
-            else Numbers[num]=1;
-            
-            if(Numbers.count(196-num))cnt+=Numbers[196-num];
+            else Numbers[num] = 1;
         }
-        cout << "Case #" << Case << " : " <<  cnt << endl;
+        cout << "Case #" << Case << " : " << cnt << endl;
     }
     return 0;
 }
